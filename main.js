e454f0931b80fe0f105dcc3e791da620bb8fe120
Valeri <mylenovo21072017@gmail.com>
2024-07-06 14:52:49 +0300

    fix html & css

---
+++ main.js
@@ -0,0 +1,11 @@
+const objC = { c: "objC prop" };
+
+const objB = Object.create(objC);
+objB.b = "objB prop";
+
+const objA = Object.create(objB);
+objA.a = "objA prop";
+
+console.log(objA); // { a: "objA prop", [[Prototype]]: objB }
+console.log(objB); // { b: "objB prop", [[Prototype]]: objC }
+console.log(objC); // { c: "objC prop", [[Prototype]]: Object }
