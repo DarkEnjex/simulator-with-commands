a8cfcd6da950c6303af2183beaaab3d383f59836
Valeri <mylenovo21072017@gmail.com>
2024-07-07 11:31:40 +0300

    ООП

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-const objC = { c: "objC prop" };
+// const objC = { c: "objC prop" };
 
-const objB = Object.create(objC);
-objB.b = "objB prop";
+// const objB = Object.create(objC);
+// objB.b = "objB prop";
 
-const objA = Object.create(objB);
-objA.a = "objA prop";
+// const objA = Object.create(objB);
+// objA.a = "objA prop";
 
-console.log(objA); // { a: "objA prop", [[Prototype]]: objB }
-console.log(objB); // { b: "objB prop", [[Prototype]]: objC }
-console.log(objC); // { c: "objC prop", [[Prototype]]: Object }
+// console.log(objA); // { a: "objA prop", [[Prototype]]: objB }
+// console.log(objB); // { b: "objB prop", [[Prototype]]: objC }
+// console.log(objC); // { c: "objC prop", [[Prototype]]: Object }
+
+// *** ООП ***
+const employee = {
+    baseSalary: 30000,
+    overtime: 10,
+    rate: 20,
+    getWage() {
+        return this.baseSalary + this.overtime * this.rate;
+    },
+};
+
+
+console.log(employee.getWage());
