bf366014fead20fbdff8be8caa5d07db002a84c3
Valeri <mylenovo21072017@gmail.com>
2024-08-05 11:05:16 +0300

    module 4 compleat

--- js/modul-4.js
+++ js/modul-4.js
@@ -123,3 +123,276 @@
 //     console.log(book[key]); // Значення властивості
 // }
 
+
+// const books = [
+//     {
+//         title: "The Last Kingdom",
+//         author: "Bernard Cornwell",
+//         rating: 8.38,
+//     },
+//     {
+//         title: "Beside Still Waters",
+//         author: "Robert Sheckley",
+//         rating: 8.51,
+//     },
+//     {
+//         title: "The Dream of a Ridiculous Man",
+//         author: "Fyodor Dostoevsky",
+//         rating: 7.75,
+//     }
+// ];
+
+
+// for (const book of books) {
+//     console.log(book); // Об'єкт книги
+//     console.log(book.title); // Назва
+//     console.log(book.author); // Автор
+//     console.log(book.rating); // Рейтинг
+// }
+
+
+// const fruits = [
+//     { name: 'Apple', color: 'Red' },
+//     { name: 'Banana', color: 'Yellow' },
+//     { name: 'Grape', color: 'Purple' },
+//     { name: 'Orange', color: 'Orange' },
+//     { name: 'Lemon', color: 'Yellow' }
+// ];
+
+// const fruitNames = [];
+// const fruitColors = [];
+
+// // Твій код тут
+// for (const fruit of fruits) {
+//     fruitNames.push(fruit.name);
+//     fruitColors.push(fruit.color);
+// }
+
+// console.log(fruitNames)
+// console.log(fruitColors)
+
+// console.log("-----------------------------")
+
+// const books = [
+//     { title: "The Last Kingdom", author: "Bernard Cornwell" },
+//     { title: "Beside Still Waters", author: "Robert Sheckley" },
+//     { title: "The Dream of a Ridiculous Man", author: "Fyodor Dostoevsky" }
+// ];
+
+// const authorToSearchFor = "Robert Sheckley";
+
+// for (const book of books) {
+//     if (book.author === authorToSearchFor) {
+//         console.log(book);
+//         console.log(book.title)
+//         console.log(book.rating)
+//     }
+// }
+
+
+// console.log("---------------------------------")
+
+// function getProductPrice(productName) {
+//     const products = [
+//         { name: 'Laptop', price: 1000, quantity: 5 },
+//         { name: 'Phone', price: 500, quantity: 10 },
+//         { name: 'Tablet', price: 300, quantity: 7 },
+//         { name: 'Headphones', price: 100, quantity: 15 }
+//     ];
+
+//     // Твій код тут
+//     for (const product of products) {
+//         if (product.name === productName) {
+//             return product.price;
+//         }
+//     }
+//     return null;
+
+// }
+
+// console.log(getProductPrice('Phone')); // 500
+// console.log(getProductPrice('Laptop')); // 1000
+// console.log(getProductPrice('Camera')); // null
+
+// console.log("------------------")
+
+// const books = [
+//     { title: "The Last Kingdom", author: "Bernard Cornwell", rating: 8.2 },
+//     { title: "Beside Still Waters", author: "Robert Sheckley", rating: 9 },
+//     { title: "The Dream of a Ridiculous Man", author: "Fyodor Dostoevsky", rating: 6.8 }
+// ];
+
+// const titles = [];
+
+// for (const book of books) {
+//     titles.push(book.title)
+// }
+
+// console.log(titles); // ["The Last Kingdom", "Beside Still Waters", "The Dream of a Ridiculous Man"]
+
+
+//---------------------------------
+// function getAllPropValues(propName) {
+//     const products = [
+//         { name: "Radar", price: 1300, quantity: 4 },
+//         { name: "Scanner", price: 2700, quantity: 3 },
+//         { name: "Droid", price: 400, quantity: 7 },
+//         { name: "Grip", price: 1200, quantity: 9 },
+//     ];
+
+//     const values = [];
+//     for (const product of products) {
+
+//     }
+// }
+
+// const atTheOldToad = {
+//     potions: [],
+//     getPotions() {
+//         return "List of all avalible potions"
+//     },
+//     addPotions(potionName) {
+//         return `Adding ${potionName}`;
+//     }
+// }
+
+// console.log(atTheOldToad.getPotions())
+// console.log(atTheOldToad.addPotions("Invisibility"))
+// console.log(atTheOldToad.addPotions("Power potion"))
+
+// const bookShelf = {
+//     books: ["The Last Kingdom", "The Mist"],
+//     getBooks() {
+//         console.log(this);
+//     },
+// };
+
+// console.log(bookShelf.getBooks()); // {books: ["The Last Kingdom", "The Mist"], getBooks: f}
+
+// const bookShelf = {
+//     books: ["The Last Kingdom", "The Mist"],
+//     getBooks() {
+//         return this.books;
+//     }
+// };
+
+// console.log(bookShelf.getBooks()); // ["The Last Kingdom", "The Mist"]
+
+// const atTheOldToad = {
+//     potions: ["Speed potion", "Stone skin"],
+//     getPotions() {
+//         return this.potions
+//     }
+// }
+
+// console.log(atTheOldToad.getPotions())
+
+// const atTheOldToad = {
+//     potions: ["Speed potion", "Stone skin"],
+//     getPotions() {
+//         return this.potions;
+//     },
+//     addPotion(potionName) {
+//         this.potions.push(potionName)
+//     }
+// }
+
+// console.log(atTheOldToad.getPotions())
+// atTheOldToad.addPotion("Invisibility")
+// atTheOldToad.addPotion("Power potion")
+// console.log(atTheOldToad.getPotions())
+
+//! Modul 4 task 1
+// function isEnoughCapacity(products, containerSize) {
+//     let totalProducts = 0;
+//     for (const product in products) {
+//         totalProducts += products[product];
+//     }
+//     return totalProducts <= containerSize;
+// }
+
+// console.log(
+//     isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+// ); // true
+
+// console.log(
+//     isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+// ); // false
+
+// console.log(
+//     isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+// ); // true
+
+// console.log(
+//     isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+// ); // false
+
+
+//! Modul 4 task 2
+// function calcAverageCalories(days) {
+//     if (days.length === 0) {
+//         return 0;
+//     }
+
+//     let totalCalories = 0;
+//     for (let day of days) {
+//         totalCalories += day.calories;
+//     }
+//     let averageTotalCalories = totalCalories / days.length;
+//     return averageTotalCalories;
+// }
+
+// console.log(
+//     calcAverageCalories([
+//         { day: "monday", calories: 3010 },
+//         { day: "tuesday", calories: 3200 },
+//         { day: "wednesday", calories: 3120 },
+//         { day: "thursday", calories: 2900 },
+//         { day: "friday", calories: 3450 },
+//         { day: "saturday", calories: 3280 },
+//         { day: "sunday", calories: 3300 }
+//     ])
+// ); // 3180
+
+// console.log(
+//     calcAverageCalories([
+//         { day: "monday", calories: 2040 },
+//         { day: "tuesday", calories: 2270 },
+//         { day: "wednesday", calories: 2420 },
+//         { day: "thursday", calories: 1900 },
+//         { day: "friday", calories: 2370 },
+//         { day: "saturday", calories: 2280 },
+//         { day: "sunday", calories: 2610 }
+//     ])
+// ); // 2270
+
+// console.log(
+//     calcAverageCalories([])
+// ); // 0
+
+//! Modul 4 task 3
+// const profile = {
+//     username: "Jacob",
+//     playTime: 300,
+
+//     changeUsername(newName) {
+//         this.username = newName;
+//     },
+
+//     updatePlayTime(hours) {
+//         this.playTime += hours;
+//     },
+
+//     getInfo() {
+//         return `${this.username} has ${this.playTime} active hours!`
+//     }
+// };
+
+
+// console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+// profile.changeUsername("Marco");
+// console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+// profile.updatePlayTime(20);
+// console.log(profile.getInfo()); // "Marco has 320 active hours!"
