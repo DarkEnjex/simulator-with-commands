d559157a78ccf1b8229de44a42bcc6320e0fa57c
Valeri <mylenovo21072017@gmail.com>
2024-08-08 21:04:07 +0300

    modul-6

---
+++ js/modul-6.js
@@ -0,0 +1,42 @@
+'use strict'
+
+// class User {
+//     constructor(name, email) {
+//         console.log(name, email);
+//     }
+// }
+
+// const mango = new User("Mango", "mango@mail.com"); // "Mango mango@mail.com"
+// console.log(mango); // {}
+
+// class User {
+//     constructor(name, email) {
+//         // Ініціалізація властивостей екземпляра
+//         this.name = name;
+//         this.email = email;
+//     }
+// }
+
+// const mango = new User("Mango", "mango@mail.com");
+// console.log(mango); // { name: 'Mango', email: 'mango@mail.com' }
+
+// const poly = new User("Poly", "poly@mail.com");
+// console.log(poly); // { name: 'Poly', email: 'poly@mail.com' }
+
+
+// class User {
+//     constructor(params) {
+//         this.name = params.name;
+//         this.email = params.email;
+//     }
+
+//     getEmail() {
+//         return this.email;
+//     }
+
+//     changeEmail(newEmail) {
+//         this.email = newEmail;
+//     }
+// }
+
+// console.log(User.prototype); // {constructor: ƒ, getEmail: ƒ, changeEmail: ƒ}
